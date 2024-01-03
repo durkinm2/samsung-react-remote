321e5e530bb8f4c02cdeab962c3d01894873392d
Michael Durkin <10467386+durkinm2@users.noreply.github.com>
2024-01-03 17:34:11 -0500

    removed manual lib

--- src/lib/samsung-api/index.ts
+++
@@ -1,7 +0,0 @@
-import Samsung from './samsung'
-
-export { default as Samsung } from './samsung'
-export { default as KEYS } from './keys'
-export { default as APPS } from './apps'
-export { default as AutoSearch } from './auto-search'
-export default Samsung
